6f3a98baefb9745d50a98d02d80dad86adb7096e
Evgeniy Pasko <pasko.evg@gmail.com>
2021-12-23 19:11:22 +0300

    The base view now returns minimal HTML

--- lists/tests.py
+++ lists/tests.py
@@ -1,3 +1,4 @@
+from django.http import HttpRequest
 from django.test import TestCase
 from django.urls import resolve
 
@@ -11,3 +12,12 @@ class HomePageTest(TestCase):
         """ Тест: Корневой url преобразуется в представление домашней страницы """
         found = resolve('/')
         self.assertEqual(found.func, home_page)
+
+    def test_home_page_returns_correct_html(self):
+        """ Тест: Домашняя страница возвращает правильный результат """
+        request = HttpRequest()
+        response = home_page(request)
+        html = response.content.decode('utf8')
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>To-Do lists</title>', html)
+        self.assertTrue(html.endswith('</html>'))
--- lists/views.py
+++ lists/views.py
@@ -1,6 +1,8 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 
 # Create your views here.
-def home_page():
-    return None
+def home_page(request):
+    """ Домашняя страница """
+    return HttpResponse('<html><title>To-Do lists</title></html>')
