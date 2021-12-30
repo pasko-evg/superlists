c10cd6468cd158b16ab58bb70d534c62566f6712
Evgeniy Pasko <pasko.evg@gmail.com>
2021-12-30 12:03:50 +0300

    New URL + view to add to existing lists. FT passes :-)

--- lists/tests.py
+++ lists/tests.py
@@ -51,19 +51,26 @@ class ListViewTest(TestCase):
 
     def test_uses_list_template(self):
         """ Тест: Используется шаблон списка """
-        response = self.client.get('/lists/best-list-app/')
+        list_ = List.objects.create()
+        response = self.client.get(f'/lists/{list_.id}/')
         self.assertTemplateUsed(response, 'list.html')
 
-    def test_displays_all_items(self):
-        """ Тест: отображаются все элементы списка """
-        list_ = List.objects.create()
-        Item.objects.create(text='Item 1', list=list_)
-        Item.objects.create(text='Item 2', list=list_)
+    def test_displays_only_items_for_that_list(self):
+        """ Тест: отображаются элементы только для этого списка """
+        correct_list = List.objects.create()
+        Item.objects.create(text='Item 1', list=correct_list)
+        Item.objects.create(text='Item 2', list=correct_list)
 
-        response = self.client.get('/lists/best-list-app/')
+        other_list = List.objects.create()
+        Item.objects.create(text='Item 3', list=other_list)
+        Item.objects.create(text='Item 4', list=other_list)
+
+        response = self.client.get(f'/lists/{correct_list.id}/')
 
         self.assertContains(response, 'Item 1')
         self.assertContains(response, 'Item 2')
+        self.assertNotContains(response, 'Item 3')
+        self.assertNotContains(response, 'Item 4')
 
     def test_can_save_a_POST_request(self):
         """ Тест: можно сохранить пост запрос """
@@ -72,7 +79,57 @@ class ListViewTest(TestCase):
         new_item = Item.objects.first()
         self.assertEqual(new_item.text, 'A new list item')
 
+    def test_displays_all_items(self):
+        """ Тест: отображаются все элементы списка """
+        list_ = List.objects.create()
+        Item.objects.create(text='Item 1', list=list_)
+        Item.objects.create(text='Item 2', list=list_)
+
+        response = self.client.get(f'/lists/{list_.id}/')
+
+        self.assertContains(response, 'Item 1')
+        self.assertContains(response, 'Item 2')
+
     def test_redirect_after_POST(self):
         """ Тест: перенаправление после post-запроса """
         response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
-        self.assertRedirects(response, '/lists/best-list-app/')
+        new_list = List.objects.first()
+        self.assertRedirects(response, f'/lists/{new_list.id}/')
+
+    def test_passes_correct_list_to_template(self):
+        """ Тест: Передается правильный шаблон списка """
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+        response = self.client.get(f'/lists/{correct_list.id}/')
+        self.assertEqual(response.context['list'], correct_list)
+
+
+class NewItemTest(TestCase):
+    """ Тест нового элемента списка """
+
+    def test_can_save_a_POST_request_to_an_existing_list(self):
+        """ Тест: можно сохранить запрос в существующий список """
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        self.client.post(
+            f'/lists/{correct_list.id}/add_item',
+            data={'item_text': 'A new item for an existing list'}
+        )
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new item for an existing list')
+        self.assertEqual(new_item.list, correct_list)
+
+    def test_redirects_to_list_view(self):
+        """ Тест: Переадресуется в представление списка """
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        response = self.client.post(
+            f'/lists/{correct_list.id}/add_item',
+            data={'item_text': 'A new item for an existing list'}
+        )
+
+        self.assertRedirects(response, f'/lists/{correct_list.id}/')
--- superlists/urls.py
+++ superlists/urls.py
@@ -21,6 +21,6 @@ from lists import views
 urlpatterns = [
     path('', views.home_page, name='home'),
     path('lists/new', views.new_list, name='new_list'),
-    path('lists/best-list-app/', views.view_list, name='view_list'),
-    path('admin/', admin.site.urls),
+    path('lists/<str:list_id>/', views.view_list, name='view_list'),
+    path('lists/<str:list_id>/add_item', views.add_item, name='add_item'),
 ]
