c994e77d22608002181c5927164cc7b5cb8e40eb
Evgeniy Pasko <pasko.evg@gmail.com>
2022-01-03 00:04:37 +0300

    Applied get_absolute_url to the list model to remove duplicate urls in views (DRY rule)

--- lists/models.py
+++ lists/models.py
@@ -2,9 +2,17 @@ from django.db import models
 
 
 # Create your models here.
+from django.urls import reverse
+
+
 class List(models.Model):
     """ Список дел """
-    pass
+    def get_absolute_url(self):
+        """
+        Получить абсолютный URL
+        :return: absolute List URL
+        """
+        return reverse('view_list', args=[self.id])
 
 
 class Item(models.Model):
