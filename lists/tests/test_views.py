c7a76ca1ca30a79233b1fad60f82ae6ce68cedb9
Evgeniy Pasko <pasko.evg@gmail.com>
2022-01-02 23:43:48 +0300

    Validation of the model in the list view is provided

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -101,6 +101,15 @@ class ListViewTest(TestCase):
         new_list = List.objects.first()
         self.assertRedirects(response, f'/lists/{new_list.id}/')
 
+    def test_validation_errors_end_up_on_lists_page(self):
+        """ Тест: Ошибки валидации оканчиваются на странице списков """
+        list_ = List.objects.create()
+        response = self.client.post(f'/lists/{list_.id}/', data={'item_text': ''})
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'list.html')
+        expected_error = escape("You can't have an empty list item")
+        self.assertContains(response, expected_error)
+
 
 class NewListTest(TestCase):
     """ Тестирование нового списка """
